33cb00ed7baca7ae7c0f18900318cd4d40a81b03
Venkatesh <belvenkatesh@gmail.com>
2020-04-17 09:56:16 +0530

    updated new student card design

--- Student.js
+++ Student.js
@@ -9,18 +9,22 @@ class Student extends React.Component {
 
 
             <View style={styles.container}>
-                <Text>Name: {this.props.name}</Text>
-                <Text>Roll No: {this.props.rollNumber}</Text>
-                <Text>DOB: {this.props.dob}</Text>
-                <Text>Address: {this.props.address}</Text>
-                <Text>Phone Number: {this.props.phoneNumber}</Text>
                 <Image
-                    style={styles.tinyLogo}
+                    style={styles.photo}
                     // source={{
                     //     uri: this.props.photoUrl,
                     // }}
                     source={this.props.photoUrl}
                 />
+                <View>
+                    <Text style={styles.title}>{this.props.name}</Text>
+                    <Text style={styles.detail}>{this.props.rollNumber}</Text>
+                </View>
+                <View>
+                    <Text style={styles.detail}>DOB: {this.props.dob}</Text>
+                    <Text style={styles.detail}>Address: {this.props.address}</Text>
+                    <Text style={styles.detail}>Phone Number: {this.props.phoneNumber}</Text>
+                </View>
             </View>
 
 
@@ -34,10 +38,37 @@ export default Student
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: 10,
+        marginBottom: 10,
+        backgroundColor: '#ebeae8',
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.41,
+        shadowRadius: 9.11,
+
+        elevation: 14,
+
+    },
+    photo: {
+        width: 300,
         height: 200
     },
-    tinyLogo: {
-        width: 50,
-        height: 50,
-      }
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 10
+    },
+    detail: {
+        color: 'gray',
+        textAlign: 'center',
+        marginTop: 10,
+        fontSize: 15
+    }
+
+
 });
