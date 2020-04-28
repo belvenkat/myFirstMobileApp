730a5fb6214b6011a64024e6861089242e139575
Venkatesh <belvenkatesh@gmail.com>
2020-04-28 15:30:42 +0530

    added position to image
    Co-authored-by: Vikram Belde <belvikram@gmail.com>

--- ThirdUiSampleCard.js
+++ ThirdUiSampleCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Dimensions, Button, Alert, TouchableHighlight, Image } from 'react-native';
+import { StyleSheet, View, Text, Dimensions, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const windowWidth = Dimensions.get('window').width;
@@ -11,35 +11,30 @@ export default function ThirdUiSampleCard() {
         <View>
             <View style={venkyStyles.Container}>
                 <View style={venkyStyles.mainContainer}>
+
                     <View style={venkyStyles.firstLayer}>
+                        <Image
+                            style={venkyStyles.tinyLogo}
+                            source={{
+                                uri: 'https://1.bp.blogspot.com/-8dlm0TpcwfI/VIeY_BT5E-I/AAAAAAAAHxw/TShiR220lj4/s1600/venkatesh.jpg',
+                            }}
+                        />
+                        <View style={venkyStyles.photo}>
                             <Image
-                                style={venkyStyles.tinyLogo}
-                                source={{
-                                    uri: 'https://1.bp.blogspot.com/-8dlm0TpcwfI/VIeY_BT5E-I/AAAAAAAAHxw/TShiR220lj4/s1600/venkatesh.jpg',
-                                }}
-                            />
-                             <Image
                                 style={venkyStyles.profilePic}
                                 source={{
                                     uri: 'https://d1kkg0o175tdyf.cloudfront.net/widget/p_f211c8aaf3db-2020-01-15-20-42-05-000473.jpg',
                                 }}
                             />
+                        </View>
                     </View>
-
-
-
                     <View style={venkyStyles.secondLayer}>
                         <View style={venkyStyles.personDetails}>
                             <Text style={venkyStyles.name}>Bel Venkatesh</Text>
                             <Icon style={venkyStyles.iconStyles} name="map-marker" size={30}></Icon>
                             <Text style={venkyStyles.place}>Kalyan nagar,HYD</Text>
                         </View>
-
-
                     </View>
-
-
-
                     <View style={venkyStyles.thirdLayer}>
                         <View style={venkyStyles.postDetailsContainers}>
                             <View style={venkyStyles.postDetailsAligniteams}>
@@ -71,20 +66,31 @@ var venkyStyles = StyleSheet.create({
     Container: {
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#1e373c',
+        backgroundColor: 'green',
         height: windowHeight,
         width: windowWidth
     },
-    mainContainer: { 
+    mainContainer: {
+        borderRadius: 10,
         backgroundColor: 'white',
         height: 300,
-        width: 500
+        width: 500,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+
+        elevation: 8,
     },
     firstLayer: {
         flex: 2,
-        backgroundColor: 'red'
     },
     tinyLogo: {
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
         height: windowHeight / 4.7,
         width: windowWidth / 2.048
     },
@@ -92,6 +98,7 @@ var venkyStyles = StyleSheet.create({
         flexDirection: 'row',
         flex: 0.5,
         backgroundColor: '#f2f4f3',
+        zIndex: -10
     },
     personDetails: {
         flexDirection: 'row',
@@ -115,14 +122,11 @@ var venkyStyles = StyleSheet.create({
     thirdLayer: {
         flexDirection: 'row',
         flex: 1.2,
-        //backgroundColor: 'tan'
     },
     postDetailsContainers: {
-        borderTopWidth: 1,
         borderColor: 'lightgray',
         borderEndWidth: 1,
         flex: 1,
-        // backgroundColor:'blue'
     },
     postDetailsAligniteams: {
         alignItems: 'center',
@@ -136,15 +140,17 @@ var venkyStyles = StyleSheet.create({
         fontSize: 20,
         color: '#858587',
     },
-    profilePic:{
-        borderColor:'white',
-        borderWidth:2,
-        height:100,
-        width:100,
-        borderRadius:50,
-        position:'absolute',
-        top:110,
-        left:200,
+    profilePic: {
+        borderColor: 'white',
+        borderWidth: 2,
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+    },
+    photo: {
+        position: 'absolute',
+        top: 120,
+        left: 200
     }
 
 })
